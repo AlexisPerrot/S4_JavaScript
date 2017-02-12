b621d384896dc79b5ca2a96f783078ff0a414691
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-02-12 14:50:38 +0100

    Initialisation
    Initialisation des fichiers js et html

---
+++ TP1/script.js
@@ -0,0 +1,30 @@
+function checkPrenom(){
+  return (document.getElementById("prenom").length<12 && document.getElementById("prenom")) 
+}
+function checkValider(){
+  if(){
+    if(){
+      if(){
+        if(){
+          if(){
+            if(){
+              if(){
+                if(){
+                  document.getElementById("ok").disabled="false";
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+document.getElementById("prenom").addEventListener("change",checkValider);
+document.getElementById("nom").addEventListener("change",checkValider);
+document.getElementById("age").addEventListener("change",checkValider);
+document.getElementById("identifiant").addEventListener("change",checkValider);
+document.getElementById("mdp").addEventListener("change",checkValider);
+document.getElementById("mdp2").addEventListener("change",checkValider);
+document.getElementById("cgu").addEventListener("change",checkValider);
