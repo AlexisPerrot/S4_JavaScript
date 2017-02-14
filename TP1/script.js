f27d235ab44c3fb7f117c0f9eeefe170fa9b5cb3
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-02-14 20:04:34 +0100

    Finalisation

--- TP1/script.js
+++ TP1/script.js
@@ -1,12 +1,32 @@
 function checkPrenom(){
-	console.log(!/[^a-zA-Z]/.test(document.getElementById("prenom").value) && document.getElementById("prenom").value.length>=1);
-	return (!/[^a-zA-Z]/.test(document.getElementById("prenom").value) && document.getElementById("prenom").value.length>=1);
+	if((!/[^a-zA-Z]/.test(document.getElementById("prenom").value) && document.getElementById("prenom").value.length>=1)){
+		document.getElementById("e_prenom").style.display="none";
+		return 1;
+	}
+	else{
+		document.getElementById("e_prenom").style.display="inline";
+		return 0;
+	}
 }
 function checkNom(){
-	return (!/[^a-zA-Z]/.test(document.getElementById("nom").value) && document.getElementById("nom").value.length>=1);
+	if((!/[^a-zA-Z]/.test(document.getElementById("nom").value) && document.getElementById("nom").value.length>=1)){
+		document.getElementById("e_nom").style.display="none";
+		return 1;
+	}
+	else{
+		document.getElementById("e_nom").style.display="inline";
+		return 0;
+	}
 }
 function checkAge(){
-	return ((/[0-9]/.test(document.getElementById("age").value)) && (document.getElementById("age").value)>=18);
+	if((/[0-9]/.test(document.getElementById("age").value)) && (document.getElementById("age").value)>=18){
+		document.getElementById("e_age").style.display="none";
+		return 1;
+	}
+	else{
+		document.getElementById("e_age").style.display="inline";
+		return 0;
+	}
 }
 function checkIdentifiant(){
 	reponse = false;
@@ -15,25 +35,66 @@ function checkIdentifiant(){
 			reponse = true;
 		}
 	}
-	return reponse;
+	if(document.getElementById("identifiant").value.length>=1 && document.getElementById("identifiant").value.length<12 && !/[^a-zA-Z]/.test(document.getElementById("identifiant").value)){
+		document.getElementById("e_identifiant").style.display="none";
+		return 1;
+	}
+	else{
+		document.getElementById("e_identifiant").style.display="inline";
+		return 0;
+	}
 }
 function checkMotDePasse(){
 	var strength = 0;
+	var maj = new RegExp("[A-Z]");
+	var min = new RegExp("[a-z]");
+	var nbr = new RegExp("[0-9]");
+	var spe = new RegExp("[^a-zA-Z0-9]");
+	
 	if (document.getElementById("mdp").value.length>=8){
-		if ((/[a-z]+[A-Z]+[0-9]/.test(document.getElementById("mdp").value))){
-			;
-		}
+		strength+=20;
 	}
-	return 1;
+	if ((maj.test(document.getElementById("mdp").value))){
+		strength+=20;
+	}
+	if ((min.test(document.getElementById("mdp").value))){
+		strength+=20;
+	}
+	if ((nbr.test(document.getElementById("mdp").value))){
+		strength+=20;
+	}
+	if ((spe.test(document.getElementById("mdp").value))){
+		strength+=20;
+	}
+	document.getElementById("lvl_mdp").innerHTML="Mot de passe sécurisé à : "+strength+"%"
+	if (strength === 100){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+
+function checkMDP2(){
+	if (document.getElementById("mdp").value===document.getElementById("mdp2").value){
+		document.getElementById("e_mdp2").style.display="none";
+		return 1;
+	}else{
+		document.getElementById("e_mdp2").style.display="inline";
+		return 0;
+	}
 }
 function checkCGU(){
 	if (document.getElementById("cgu").checked === true){
+		document.getElementById("e_cgu").style.display="none";
 		return 1;
-	}else return 0;
+	}else{
+		document.getElementById("e_cgu").style.display="inline";
+		return 0;
+	}
 }
-function setErrorMessage(message){
-	console.log(message);
-}
+
 
 function checkValider(){
 	if(checkPrenom()){
@@ -41,39 +102,44 @@ function checkValider(){
 			if(checkAge()){
 				if(checkIdentifiant()){
 					if(checkMotDePasse()){
-						if(checkCGU()){
-							document.getElementById("ok").disabled="";
-							console.log("checkValider");
+						if(checkMDP2()){
+							if(checkCGU()){
+								document.getElementById("ok").disabled="";
+							}else{
+								document.getElementById("ok").disabled="true";
+							}
 						}else{
 							document.getElementById("ok").disabled="true";
-							setErrorMessage("Vous devez accepter les CGU");
 						}
 					}else{
 						document.getElementById("ok").disabled="true";
-						setErrorMessage("Vous devez entrer un MDP corect");
 					}
 				}else{
 					document.getElementById("ok").disabled="true";
-					setErrorMessage("Vous devez entrer un identifiant correct");
 				}
 			}else{
 				document.getElementById("ok").disabled="true";
-				setErrorMessage("Vous devez entrer un age correct");
 			}
 		}else{
 			document.getElementById("ok").disabled="true";
-			setErrorMessage("Vous devez entrer un nom correct");
 		}
 	}else{
 		document.getElementById("ok").disabled="true";
-		setErrorMessage("Vous devez entrer un prénom correct");
 	}
 }
 
-document.getElementById("prenom").addEventListener("input",checkValider);
-document.getElementById("nom").addEventListener("input",checkValider);
-document.getElementById("age").addEventListener("input",checkValider);
-document.getElementById("identifiant").addEventListener("input",checkValider);
-document.getElementById("mdp").addEventListener("input",checkValider);
-document.getElementById("mdp2").addEventListener("input",checkValider);
-document.getElementById("cgu").addEventListener("input",checkValider);
+document.getElementById("prenom").addEventListener("input",checkPrenom);
+document.getElementById("nom").addEventListener("input",checkNom);
+document.getElementById("age").addEventListener("input",checkAge);
+document.getElementById("identifiant").addEventListener("input",checkIdentifiant);
+document.getElementById("mdp").addEventListener("input",checkMotDePasse);
+document.getElementById("mdp2").addEventListener("input",checkMDP2);
+document.getElementById("cgu").addEventListener("input",checkCGU);
+
+document.getElementById("prenom").addEventListener("change",checkValider);
+document.getElementById("nom").addEventListener("change",checkValider);
+document.getElementById("age").addEventListener("change",checkValider);
+document.getElementById("identifiant").addEventListener("change",checkValider);
+document.getElementById("mdp").addEventListener("change",checkValider);
+document.getElementById("mdp2").addEventListener("change",checkValider);
+document.getElementById("cgu").addEventListener("change",checkValider);
