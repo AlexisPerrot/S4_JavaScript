b936284fe68193cab4afe31e2695affd0c6afe04
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-03-05 14:20:28 +0100

    Correction partielle TP1, Début TP2

--- TP2/script.js
+++ TP2/script.js
@@ -1,5 +1,23 @@
-window.addEventListener("load", function(){
+function horlogeUpdate(){
+	var dateActuelle = new Date;
+	var secActuelle = dateActuelle.getSeconds();
+	document.getElementById("horloge").textContent = dateActuelle.getHours() + ':' + dateActuelle.getMinutes() + ':' + secActuelle;
+	if(secActuelle === "0"){
+		verifAlarmes();
+	}
+	setTimeout(horlogeUpdate, 1000);
+}
 
+function verifAlarmes(){
+	var alarmes = document.getElementsByClassName("alarme");
+	for (alarm in alarmes){
+		
+	}
+}
+
+window.addEventListener("load", function(){
+	horlogeUpdate();
+	
 })
 
  
