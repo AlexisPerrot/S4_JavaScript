23c2e772a555ca1978f3f8858752cf86d26c33cd
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-03-12 12:24:18 +0100

    TP3 Finalisation

--- TP3/script.js
+++ TP3/script.js
@@ -18,7 +18,17 @@ function changerChapitre(url){
 	req.onload = function() {
 		if (req.status === 200) {
 			var data = JSON.parse(req.responseText);
-				// do what you have to do with 'data'
+				console.log(data)
+				document.getElementById("histoire").textContent = data.txt;
+				document.getElementById("liens").innerHTML = ""
+				for(var i=0;i<data.links.length;i++){
+					var elemListe = document.createElement("li");
+					var lien = document.createElement("a");
+					lien.textContent = data.links[i].txt;
+					lien.href = data.links[i].link;
+					elemListe.appendChild(lien);
+					document.getElementById("liens").appendChild(elemListe);
+				}
 		} else {
 			console.log("Erreur " + req.status);
 		}
@@ -26,14 +36,25 @@ function changerChapitre(url){
 	req.send();
 }
 
-window.addEventListener("load", function(){
+//CheckURLCourant sert à choisir le chapitre à afficher en fonction de ce qui suit le hash de l'URL
+function checkURLCourant(){
+	var url = window.location.href;
+	console.log(url);
+	var urlsplit = url.split("#",2)[1];
+	if(urlsplit != undefined){
+		console.log(urlsplit);
+		changerChapitre("Chapitre"+urlsplit+".json");
+	}
+	else{
+		changerChapitre("chapitre1.json")
+	}
+}
 
+window.addEventListener("load", function(){
 	//Lancement de l'horloge qui se refresh à chaque seconde
 	horlogeUpdate();
-	
-	document.addEventListener("hashchange",function(){
-		//utiliser window.location.href
-		//spliter l'url par #
-		//utiliser l'url avec changerChapitre 
-	)};
-);
+	//On check l'URL une première fois au cas où l'utilisateur essaie d'accéder à une page différente de la première dès le début (marque page, lien externe,...)
+	checkURLCourant();
+	//On abonne la fonction de vérification de l'URL à l'évènement "hashchange"
+	window.addEventListener("hashchange",checkURLCourant);
+});
