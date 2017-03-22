2d4693c0fe9c28dcb65a090a9cbb0e483b9618a8
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-03-22 09:24:05 +0100

    TP4 Initialisation fonctions JS

---
+++ TP4/dessin.js
@@ -0,0 +1,32 @@
+function mainlevee(){
+	
+}
+function ligne(){
+	
+}
+function rectangle(){
+	
+}
+function cercle(){
+	
+}
+function palette(){
+	
+}
+function pipette(){
+	
+}
+function importer(){
+	
+}
+function exporter(){
+	
+}
+function effacer(){
+	
+}
+
+window.addEventListener("load", function(){
+	var canvas  = document.querySelector('#canvas');
+	var context = canvas.getContext('2d');
+});
