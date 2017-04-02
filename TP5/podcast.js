7ccd5fff7703f37b95c003796613d60ac34ee199
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-04-02 15:33:23 +0200

    TP5

--- TP5/podcast.js
+++ TP5/podcast.js
@@ -1,5 +1,34 @@
 (function (){
 	window.addEventListener("load", function(){
-
+		document.getElementById("audioList").addEventListener("click", function(evt){
+			console.log(document.getElementById(evt.target.id).lastChild.src);
+    	document.getElementById("lecteur").src = document.getElementById(evt.target.id).lastChild.src;;
+		});
+		document.getElementById("ok").addEventListener("click", function(){
+			var oReq = new XMLHttpRequest();
+			console.log(oReq);
+			var url = document.getElementById("urlPod").value;
+			console.log(url);
+			oReq.open("GET", url, true);
+			oReq.onload = function(evt) {
+				var res = oReq.responseXML; // n'est pas responseText
+				console.log(res);
+				var listeItem = res.getElementsByTagName("item");
+				console.log(listeItem);
+				var txt = "";
+				for (i = 0; i < listeItem.length; i++) {
+					var titleContent =  listeItem[i].children[1].innerHTML;
+					var url = listeItem[i].getElementsByTagName("enclosure")[0].getAttribute("url");
+					var audio = document.createElement("audio");
+					var titre = document.createElement("p");
+					titre.textContent = titleContent;
+					audio.setAttribute("src",url);
+					titre.appendChild(audio);
+					titre.setAttribute("id",i)
+					document.getElementById("audioList").appendChild(titre);
+				}
+			}
+			oReq.send();
+		});
 	});
 })();
